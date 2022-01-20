97ef1b406c016b11e52eea42615cdfa2cb56879b
shafiqahmad81 <shafiqahmad081@gmail.com>
2022-01-20 13:38:53 +0600

    pricing

---
+++ assets/js/min.js
@@ -0,0 +1,13 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function ($){
+
+        $(".video-play-btn").magnificPopup({
+            type:'video',
+        });
+
+    });
+    
+
+}(jQuery));
